ff034dd72c20985152e4d946fa6fe2d713739209
pluffie <59744842+pluffie@users.noreply.github.com>
2022-07-25 14:02:52 +0300

    Fix colors

--- lua/everblush/core.lua
+++ lua/everblush/core.lua
@@ -6,7 +6,7 @@ function M.get_colors()
     contrast = "#161d1f",
     background = "#141b1e",
     foreground = "#dadada",
-    cursorline = "#2c333",
+    cursorline = "#2c3333",
     cursor = "#dadada",
     color0 = "#232a2d",
     color1 = "#e57474",
@@ -22,7 +22,7 @@ function M.get_colors()
     color11 = "#f4d67a",
     color12 = "#71baf2",
     color13 = "#ce89df",
-    color14 = "##67cbe7",
+    color14 = "#67cbe7",
     color15 = "#bdc3c2",
   }
 end
